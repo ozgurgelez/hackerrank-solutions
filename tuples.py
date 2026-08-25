5aa521f0b3a301cd291b81098f2f90a731699f7a
Mehmet Özgür Gelez <mehmetozgurgelez@gmail.com>
2026-08-25 19:59:16 +0300

    String Split and Join Solution

--- tuples.py
+++ tuples.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split())
+    n = int(raw_input()) # type: ignore
+    integer_list = map(int, raw_input().split()) # type: ignore
     t = tuple(integer_list)
     print(hash(t))
